f6b4bee51b1dde9aac3226bdc794b9e0d90258b7
samgoody <ssgoodman@gmail.com>
2010-02-15 14:01:27 +0200

    Complete rewrite of the class. More logical names and structure. Changed init to expect an id or ref to an individual element. Added image for linear BWBar Created snippets page for those snippets I wish to keep around.
    Signed-off-by: samgoody <ssgoodman@gmail.com>

--- scripts/ColorRoller.js
+++ scripts/ColorRoller.js
@@ -1,11 +1,15 @@
 var ColorRoller = new Class({
+	Implements: Options,
+	
 	options: {
-	
+		setspace: 'rgb',
+		RGB: [127,127,127]
 	},
-	
-	initialize: function(element,options){	
-		 this.build().setStyles({'height':22, 'vertical-align':'top'}).injectAfter(element);
-		 this.e.crColorRoller.addClass('crHide')
+
+	initialize: function(element,options){
+		this.setOptions(options);
+		this.build().setStyles({'height':22, 'vertical-align':'top'}).injectAfter($(element));
+		this.e.crColorRoller.addClass('crHide')
 	},
 	
 	build: function(){
@@ -19,11 +23,11 @@ var ColorRoller = new Class({
 			function(v,k){
 				new Element('option',{'value':k,'text':v,'class':'crO'+k}).inject(++i>3 ? e.crType : e.crSpace);
 			});
-		['ColorRoller','Head','Box','BoxSel','BoxSee','Bar','BarSel','Nums','Val','Shade'].each(
+		['ColorRoller','Head','Box','BoxSel','BoxSee','Bar','BarSel','Nums','Val'].each(
 			function(v){
 				e['cr'+v] = new Element('div',{'class':'cr'+v});
 			});
-		$each({R:'updateRGB',G:'updateRGB',B:'updateRGB',H:'setBox',S:'setBox',V:'setBar',Hex:'setHex'},
+		$each({R:'inputRGB',G:'inputRGB',B:'inputRGB',H:'inputHS',S:'inputHS',V:'inputV',Hex:'inputRGBHex'},
 			function(v,k){
 				e['cr' +k] = new Element('span',{'text':k,'class':'cr'+k});
 				e['crI'+k] = new Element('input',{'type':'text','class':'crI'+k});
@@ -33,8 +37,7 @@ var ColorRoller = new Class({
 		e.crColorRoller.adopt(
 			e.crHead,
 			e.crBox.adopt(
-				e.crImg.set('src','images/Transp.png'),//Color-Wheel.png
-				e.crShade,
+				e.crImg.set('src','images/Transp.png'),
 				e.crBoxSel.adopt(e.crBoxSee)
 			),
 			e.crBar.adopt(e.crBarSel),
@@ -52,46 +55,113 @@ var ColorRoller = new Class({
 	
 	addEvents: function(){
 		var self = this, els = this.e;
-		
+
 		this.barHeight = els.crBar.getSize().y;
 		this.radius = els.crBox.getSize().x / 2;
-		this.setSpace();
 		
-		els.crShade.addEvent('mousedown',this.click.bind(self));		
 		this.BoxSel = new Drag(els.crBoxSel, {
 			snap: 0,
-			onDrag: self.updateBox.bind(self)
+			onDrag: self.picker.bind(self)
 		});
 		this.BarSel = new Drag(els.crBarSel, {
 			snap: 0,
 			limit: {x:[-1,-1],y:[0,self.barHeight]},
-			onDrag: self.updateBar.bind(self)
+			onDrag: self.slider.bind(self)
 		});
-		els.crSpace.addEvent('change',this.setSpace.bind(this));
+
+		this.setRGB(this.options.RGB,3);
+		els.crImg.addEvent('mousedown',this.click.bind(self));
+		els.crSpace.addEvent('change',this.setSpace.bind(this)).fireEvent('change');
 		els.crShow.addEvent('click',this.show.bind(this));
 	},
-	setBG: function(val){
-		var v = Math.round(val * 2.55), 
-			grey = v+','+v+','+v, 
-			shape = 'radial', 
-			space = this.space;
-		val = space == 'B' ? val / 100 : 
-			space == 'L' ? 1 - Math.abs(val / 50 - 1) : 1;
-		this.e.crImg.setStyle('opacity',val);
-		this.e.crBox.setStyle('background-color', 'rgb('+grey+')'); 
+	
+	//Inputs:
+	inputRGB: function(){
+		this.setRGB(this.getValues(['R','G','B']),1);
 	},
-	updateBar: function(){
-		var val = this.val = 100 - this.e.crBarSel.getStyle('top').slice(0,-2) / this.barHeight * 100;// was e.crBarSel.getPosition(e.crBar).y 
-		this.e.crIV.set('value',Math.round(val));
-		this.setBG(val);
-		this.updateBox();
+	inputRGBHex: function(event){
+		this.setRGB(event.target.value.hexToRgb(),2);
 	},
-	setBar: function(set){
-		var val = this.val = this.e.crIV.get('value');
-		this.e.crBarSel.setStyle('top',100-val+'%');
-		this.setBG(val);
-		if(set)this.updateBox();
+	inputV: function(event){
+		this.setV(event.target.value,1);
 	},
+	inputHS: function(){
+		var v = this.getValues(['H','S']);
+		this.setHS(v[0],v[1],1);
+	},
+	slider: function(cursor){
+		this.setV(100 - cursor.getStyle('top').slice(0,-2) / this.barHeight * 100,2);
+		//Was e.crBarSel.getPosition(e.crBar).y or cursor.getPosition(arguments[1].originalTarget).y 
+	},
+	picker: function(){
+		var els = this.e;	//needs acces to e, radius, g.Since stored on object for now, needs to maintain this.
+		var pos = els.crBoxSel.getPosition(els.crBox), // can this be done cheaper using getStyle or using the event?
+			x = pos.x - this.radius,
+			y = this.radius - pos.y,
+			h = Math.atan2(x,y) * 180 / Math.PI,
+			s = Math.sqrt(x*x+y*y) * 100 / this.radius;
+			
+		if (s > 99) return this.BoxSel.stop(); 
+		if (h < 0) h -= -360;
+		this.setHS(h,s,2);
+	},
+	
+	// Set Values
+	setHS: function(H,S,step){
+		//Steps: #1 - Set inputs. #2 - Set Picker Position. #0 - Set RGB
+		if (step != 1) this.setValues({H:H,S:S});
+		if (step != 2){
+			var	radius = S * this.radius / 100,
+				angle = H * Math.PI / 180;
+			this.e.crBoxSel.setStyles({
+				top :this.radius - radius * Math.cos(angle) ,
+				left:this.radius + radius * Math.sin(angle) 
+			});
+		}
+		if (step) this.setRGB([H,S,this.val].toRgb(this.space));
+	},
+	setV: function(val,step){
+		//Steps: #1 - Set inputs. #2 - Set Slider. #3 - Set Background Shading. #0 - Set RGB
+		this.val = val;
+		var els = this.e,
+			v = Math.round(val * 2.55), 
+			grey = v+','+v+','+v,
+			value = this.space == 'B' ? val / 100 : 
+				this.space == 'L' ? 1 - Math.abs(val / 50 - 1) : 1;
+		els.crImg.setStyle('opacity',value);
+		els.crBox.setStyle('background-color', 'rgb('+grey+')'); 
+		
+		if (step != 1) els.crIV.set('value',Math.round(val));
+		if (step != 2) els.crBarSel.setStyle('top',100-val+'%');
+		if (step){
+			var rgb = this.getValues(['H','S']);
+			rgb.push(val);
+			this.setRGB(rgb.toRgb(this.space));
+		}
+	}, 
+	setRGB: function(val,step){
+		//Steps: #1 - Set inputs. #2 - Set Hex. #3 - Set View. #0 - Set HSV
+		var e = this.e;
+		this.setValues({R:val[0],G:val[1],B:val[2]});
+		e.crIHex.set('value',val.rgbToHex().toUpperCase());
+		e.crView.setStyle('background-color','rgb('+val+')');
+		if (step){
+			var hsv = val.fromRgb(this.space); 
+			this.setHS(hsv[0],hsv[1]);
+			this.setV(hsv[2]);
+		}
+	},
+	
+	// Utilities
+	setValues: function(val){
+		var self = this;
+		$each(val, function(v,k){ self.e['crI'+k].set('value',v) });
+	},
+	getValues:function(val){
+		var self = this;		
+		return val.map(function(el){ return self.e['crI'+el].get('value') });
+	},
+	
 	click:function(event){
 		this.e.crBoxSel.setPosition({
 			y:event.page.y-this.e.crBox.getPosition().y,
@@ -100,95 +170,20 @@ var ColorRoller = new Class({
 		this.BoxSel.start(event);
 		this.updateBox();
 	},
-	setHex: function(e){
-		this.updateRGB(e.target.get('value'));
-		
-		//this.e.crIHex.
-	},
-	updateBox: function(){
-		var e = this.e;	//needs acces to e, radius, g.Since stored on object for now, needs to maintain this.
-		var pos = e.crBoxSel.getPosition(e.crBox), // can this be done cheaper using getStyle?
-			x = pos.x - this.radius,
-			y = this.radius - pos.y,
-			h = Math.atan2(x,y) * 180 / Math.PI,
-			s = Math.sqrt(x*x+y*y) * 100 / this.radius;
-			
-		if (s > 99) return this.BoxSel.stop(); 
-		if (h < 0) h -= -360;
-		this.updateNums(h,s);
-	},
-	setBox:function(set){
-		var hue = this.e.crIH.get('value'),
-			sat = this.e.crIS.get('value'),
-			radius = sat * this.radius / 100,
-			angle = hue * Math.PI / 180;
-		
-		this.e.crBoxSel.setStyles({
-			top :this.radius - radius * Math.cos(angle) ,
-			left:this.radius + radius * Math.sin(angle) 
-		});
-		if(set) this.updateNums(hue,sat)
-	},
-	updateNums: function(hue,sat){
-		var rgb = [hue,sat,this.val].toRgb('hs'+this.space), e = this.e;
-		$each(
-			{R:rgb[0],G:rgb[1],B:rgb[2],H:Math.round(hue),S:Math.round(sat)},
-			function(v,k){ e['crI'+k].set('value',v) }
-		);
-		
-		e.crIHex.set('value',rgb.rgbToHex().toUpperCase());
-		e.crView.setStyle('background-color','rgb('+rgb+')');
-	},
-	updateRGB:function(color){
-		var e = this.e,
-			rgb = $type(color) != 'event'
-				? color.hexToRgb()
-				: [
-					e.crIR.get('value'),
-					e.crIG.get('value'),
-					e.crIB.get('value')
-				],
-			hsg = rgb.fromRgb('hs'+this.space);
-		$each(
-			{H:hsg[0],S:hsg[1],V:hsg[2]},
-			function(v,k){ e['crI'+k].set('value',v) }
-		);
-		if(!color)e.crIHex.set('value',rgb.rgbToHex().toUpperCase());
-		e.crView.setStyle('background-color','rgb('+rgb+')');
-		this.setBar(false);
-		this.setBox(false);
-	},
 	setSpace: function(e){
-		this.space = e ? e.target.value : 'G';
+		this.space = e.target.value;
 		this.e.crV.set('text',this.space);
-		this.updateBar();
+		this.options.setspace == 'rgb' 
+			? this.inputRGB(this.getValues(['R','G','B']))
+			: (this.inputHS(this.getValues(['H','S'])) && this.inputV(['V']));
 	},
-	
+	setPicker: function(shape){
+		this.shape = shape ? 'linear' : 'radial';
+		this.control = '';
+	},
 	show: function(){
 		this.e.crColorRoller.removeClass('crHide');
-	},
-
-	
-	boxCoords: function(){
-		var e = this.e;	//needs acces to e, radius, g.Since stored on object for now, needs to maintain this.
-		var pos = e.crBoxSel.getPosition(e.crBox), // can this be done cheaper using getStyle?
-			x = pos.x - this.radius,
-			y = this.radius - pos.y;
-		if(circle){
-			h = Math.atan2(x,y) * 180 / Math.PI,
-			s = Math.sqrt(x*x+y*y) * 100 / this.radius;
-			if (s > 99) return this.BoxSel.stop(); 
-			if (h < 0) h -= -360;
-		} else{
-			h = x; 
-			s = y;
-		}	
-		
-		this.updateNums(h,s);
-		
-		//update the gradient.
 	}
-	
 
 });
 /*
