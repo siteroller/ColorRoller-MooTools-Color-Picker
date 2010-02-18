557fd2876aed9a8c8296b358cdddbaee3726d7a5
samgoody <ssgoodman@gmail.com>
2010-02-18 09:46:52 +0200

    Changed names of H and V inputs to 0 and 1, Renamed images Further support for GIMP picker. Commented many of the revisions that had not been committed.
    Signed-off-by: samgoody <ssgoodman@gmail.com>

--- scripts/ColorRoller.js
+++ scripts/ColorRoller.js
@@ -19,7 +19,7 @@ var ColorRoller = new Class({
 			{Space:'select',Type:'select',Img:'img',Shade:'img',Show:'img',View:'span'},
 			function(v,k){ e['cr'+k] = new Element(v,{'class':'cr'+k}) }
 		);
-		$each({G:'HSG', B:'HSB/V',L:'HSL/I',CW:'Color - Wheel', CS:'Color - Square',GS:'Grey - Stretched',GL:'Grey - Literal'},
+		$each({G:'HSG', B:'HSB/V',L:'HSL/I',0:'Color Wheel', 1:'MS Paint',2:'GIMP',3:'Adobe CS'},
 			function(v,k){
 				new Element('option',{'value':k,'text':v,'class':'crO'+k}).inject(++i>3 ? e.crType : e.crSpace);
 			});
@@ -27,11 +27,12 @@ var ColorRoller = new Class({
 			function(v){
 				e['cr'+v] = new Element('div',{'class':'cr'+v});
 			});
-		$each({R:'inputRGB',G:'inputRGB',B:'inputRGB',H:'inputHS',S:'inputHS',V:'inputV',Hex:'inputRGBHex'},
+		$each({R:'inputRGB',G:'inputRGB',B:'inputRGB',0:'input0',S:'inputS',1:'input1',Hex:'inputRGBHex'},
 			function(v,k){
-				e['cr' +k] = new Element('span',{'text':k,'class':'cr'+k});
+				if (k==0) k = 0; // Yes, I know this is odd.
+				e['cr' +k] = new Element('span',{'text':k||'H','class':'cr'+k||'H'});
 				e['crI'+k] = new Element('input',{'type':'text','class':'crI'+k});
-				if (v) e['crI'+k].addEvent('keyup',self[v].bind(self)); // is the if(v) check needed?
+				e['crI'+k].addEvent('keyup',self[v].bind(self));
 			});
 		
 		e.crColorRoller.adopt(
@@ -44,7 +45,8 @@ var ColorRoller = new Class({
 			e.crBar.adopt(e.crBarSel),
 			e.crNums.adopt(
 				e.crSpace,e.crType,e.crVal.adopt(
-					e.crR,e.crIR,e.crG,e.crIG,e.crB,e.crIB,e.crH,e.crIH,e.crS,e.crIS,e.crV,e.crIV
+					e.crR,e.crIR,e.crG,e.crIG,e.crB,e.crIB,e.cr0,e.crI0,e.crS,e.crIS,e.cr1,e.crI1
+					//e.crR,e.crIR,e.crG,e.crIG,e.crB,e.crIB,e.crH,e.crIH,e.crS,e.crIS,e.crV,e.crIV
 				),
 				e.crHex,e.crIHex,e.crView
 			)
@@ -60,6 +62,7 @@ var ColorRoller = new Class({
 		this.boxHeight = els.crBox.getSize().y;
 		this.radius = els.crBox.getSize().x / 2;
 		
+		this.modify = 1;
 		this.setRGB(this.options.RGB,3);
 		els.crSpace.addEvent('change',this.setSpace.bind(this)).fireEvent('change');
 		els.crType.addEvent('change',this.setType.bind(this)).fireEvent('change');
@@ -88,18 +91,35 @@ var ColorRoller = new Class({
 		this.setRGB(this.getValues(['R','G','B']),1);
 	},
 	inputRGBHex: function(event){
-		this.setRGB(event.target.value.hexToRgb(),2);
+		this.setRGB(event.target.value.hexToRgb(1),2);
 	},
-	inputV: function(event){
-		this.setV(event.target.value,1);
+	input0: function(event){
+		this.inputHSV(0,event.target.value);
+		//this.modify 
+		//	? this.setHS(event.target.value,this.e.crS.get('value'),1)
+		//	: this.setV(event.target.value,1);
 	},
-	inputHS: function(){
-		var v = this.getValues(['H','S']);
-		this.setHS(v[0],v[1],1);
+	inputS: function(event){
+		this.setHS( this.e['crI'+ +(!this.modify)].get('value'), event.target.value, 1)
+	},
+	input1: function(event){
+		this.inputHSV(1,event.target.value);
+		//this.modify 
+		//	? this.setV(event.target.value,1)
+		//	: this.setHS(event.target.value,this.e.crS.get('value'),1);
+		//var v = this.getValues(['H','S']);
+		//var v = this.getValues([0,'S']);
+		//this.setHS(v[0],v[1],1);
+	},
+	inputHSV: function(el,val){
+		//console.log('this.modify == el', this.modify,el,  this.modify == el )
+		this.modify != el 
+			? this.setHS(val,this.getValues(['S']),1)
+			: this.setV(val,1);
 	},
 	slider: function(event){
-		var val = 100 - 100 * (event.page.y - this.offset.y) / this.barHeight
-		if(this.mousedown && (val > -1) && (val < 101))this.setV(val, 2);
+		var val = 100 - 100 * (event.page.y - this.offset.y) / this.barHeight;
+		if(this.mousedown && (val > -1) && (val < 101)) this.setV(val, 2);
 	},
 	picker: function(event){
 		if (!this.mousedown) return;
@@ -107,7 +127,7 @@ var ColorRoller = new Class({
 			X = event.page.x - this.offset.x,
 			Y = event.page.y - this.offset.y;
 		
-		if (this.shape == 'CW'){
+		if (!this.type){
 			var O = X - this.radius,
 				A = this.radius - Y;
 			H = Math.atan2(O,A) * 180 / Math.PI;
@@ -115,9 +135,9 @@ var ColorRoller = new Class({
 			if (S > 100) return;
 			if (H < 0) H -= -360;
 		} else {
-			S = 100 - Y * 100 / this.boxHeight; 
-			if (this.shape == 'CS') H = X * 360 / this.boxHeight;
-			else if (this.shape == '2') H = X * 360 / (this.space == 'G' ? this.boxHeight : Y);
+			var perc = this.modify ? 360 : 100;
+			S = 100 - Y * 100 / this.boxHeight;
+			H = X * perc / (this.type == 1 || this.space == 'G' ? this.boxHeight : Y);
 		}
 		
 		els.crBoxSel.setPosition({
@@ -128,76 +148,91 @@ var ColorRoller = new Class({
 	},
 	
 	// Set Values
-	setHS: function(H,S,step){
-		//Steps: #1 - Set inputs. #2 - Set Picker Position. #0 - Set RGB
-		if (step != 1) this.setValues({H:H,S:S});
-		if (step != 2){
-			if(this.shape == 'CW'){
-				var	angle = H * Math.PI / 180,
-					radius = S * this.radius / 100,
-					top  = this.radius - radius * Math.cos(angle),
-					left = this.radius + radius * Math.sin(angle);
-			} else if(this.shape == 'CS'){
-				var top  = S * this.boxHeight / 100,
-					left = H * this.boxHeight / 360 ;
-			}
-			this.e.crBoxSel.setStyles({top:top, left:left});
-		}
-		if (step) this.setRGB([H,S,this.val].toRgb(this.space));
-	},
-	setV: function(val,step){
-		//Steps: #1 - Set inputs. #2 - Set Slider. #3 - Set Background Shading. #0 - Set RGB
-		this.val = val;
-		var els = this.e,
-			v = Math.round(val * 2.55), 
-			grey = v+','+v+','+v,
-			value = this.space == 'B' ? val / 100 : 
-				this.space == 'L' ? 1 - Math.abs(val / 50 - 1) : 1;
-		els.crImg.setStyle('opacity',value);
-		els.crBox.setStyle('background-color', 'rgb('+grey+')'); 
-		
-		if (step != 1) els.crIV.set('value',Math.round(val));
-		els.crBarSel.setStyle('top',100-val+'%');
-		if (step){
-			var rgb = this.getValues(['H','S']);
-			rgb.push(val);
-			this.setRGB(rgb.toRgb(this.space));
-		}
-	}, 
 	setRGB: function(val,step){
 		//Steps: #1 - Set inputs. #2 - Set Hex. #3 - Set View. #0 - Set HSV
 		var e = this.e;
-		this.setValues({R:val[0],G:val[1],B:val[2]});
+		//console.log('setRGB - val',val)
+		this.setValues(['R','G','B'],val);
 		e.crIHex.set('value',val.rgbToHex().toUpperCase());
 		e.crView.setStyle('background-color','rgb('+val+')');
 		if (step){
-			var hsv = val.fromRgb(this.space); 
+			var hsv = val.fromRgb(this.space);
+				//console.log(hsv)
 			this.setHS(hsv[0],hsv[1]);
 			this.setV(hsv[2]);
 		}
 	},
+	setHS: function(H,S,step){
+		//console.log(arguments, 'arguments')
+		//Steps: #1 - Set inputs. #2 - Set Picker Position. #0 - Set RGB
+		if (step != 1) this.setValues([+(!this.modify),'S'],[H,S],5);
+		if (step != 2){
+			if(!this.type){
+				var	angle = H * Math.PI / 180,
+					radius = S * this.radius / 100,
+					top  = this.radius - radius * Math.cos(angle),
+					left = this.radius + radius * Math.sin(angle);
+			} else { //if(this.type == 1)
+				var top  = this.boxHeight - S * this.boxHeight / 100,
+					left = H * this.boxHeight / (this.modify ? 360 : 100);
+			}
+			this.e.crBoxSel.setStyles({top:top, left:left});
+		}
+		if (step){ 
+			var hsv = this.modify
+				? [H,S,this.val]
+				: [this.val * 3.59,S,H];// that order should only be if modify is true
+				//console.log('hsv',hsv)
+			//hsv[this.modify ? 'push' : 'unshift'](); // that 3.59 should only be if modify is true
+			//console.log(hsv)
+			this.setRGB(hsv.toRgb(this.space));
+		}
+	},
+	setV: function(val,step){
+		var els = this.e;
+		//console.log('setvV - val',val)
+		// Set Opacity if slider is greyscale. 
+		if (this.modify){
+			var v = Math.round(val * 2.55), 
+			grey = [v,v,v],
+			value = this.space == 'B' ? val / 100 : 
+				this.space == 'L' ? 1 - Math.abs(val / 50 - 1) : 1;
+			els.crImg.setStyle('opacity',value);
+		} else var V = val * 3.59, grey = [V,100,100].toRgb();
+		
+		//Set this.val. Set Slider. Set BG Color. #1 - Set inputs. #0 - Set RGB
+		this.val = val;
+		els.crBarSel.setStyle('top',100-val+'%');
+		els.crBox.setStyle('background-color', 'rgb('+grey+')'); 
+		if (step != 1) els['crI'+this.modify].set('value', Math.round(V||val));
+		if (step) this.setRGB(this.getValues([0,'S',1]).toRgb(this.space));
+	}, 
 	
 	// Utilities
 	getValues:function(val){
 		var self = this;		
 		return val.map(function(el){ return self.e['crI'+el].get('value') });
 	},
-	setValues: function(val){
+	setValues: function(key,val){
 		var self = this;
-		$each(val, function(v,k){ self.e['crI'+k].set('value',Math.round(v)) });
+		//console.log('val',val)
+		val.each(function(v,i){ self.e['crI'+key[i]].set('value',Math.round(v)) });
 	},
 	setSpace: function(e){
 		this.space = e ? e.target.value : 'G';
-		this.e.crV.set('text',this.space);
+		this.e.cr1.set('text',this.space);
 		this.options.setspace == 'rgb' 
 			? this.inputRGB()
 			: (this.setV(this.getValues(['V'])[0]) && this.inputHS());
 	},
 	setType: function(e){
-		var shape = this.shape =  e ? e.target.value : 'CW';
-		this.e.crShade.set('src',shape == 'CW' ? 'images/border.png' : '');
-		if (shape == 'CS') this.e.crImg.set('src','images/rainbow2.png');
-		else if(shape == 'CW') this.e.crImg.set('src','images/Transp.png');
+		var type = this.type = e ? e.target.value : 0,
+			img = ['wheel','paint','gimp','adobe'];
+		this.modify = +(type < 2);
+		this.e.crShade.set('src',!type ? 'images/border.png' : '');
+		this.e.crImg.set('src','images/'+img[type]+'.png');
+		this.e.crBox.setStyle('background-color','');
+		this.inputRGB();
 	},
 	show: function(){
 		this.e.crColorRoller.removeClass('crHide');
@@ -212,7 +247,6 @@ var ColorRoller = new Class({
 /*
 //todo
 1. Add support for other browsers
-2. Add support for other shapes besides sphere.
+2. Add support for Adobe typ color picker.
 3. Allow for smaller color wheel, with just a wheel.
-4. Allow for hex to be manually updated.
 */
